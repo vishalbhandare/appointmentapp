4b2ce374149d298c9ebca0461a16e0d954241c1c
Vishal Bhandare <vbhandare@webuy.com>
2020-05-03 23:32:20 +0530

    Changes to be committed: 	modified:   src/constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
-export const apiServiceUrl = 'http://localhost:8000';
+export const apiServiceUrl = 'https://obscure-gorge-40760.herokuapp.com';
 export const clientID = 3;
 export const clientSecretKey= 'qDP0Ym6aJlJkjUuD0TEjI97yF5fiIjmLG516Zcdd';
 export const scope = '*';

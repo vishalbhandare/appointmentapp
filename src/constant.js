32ad279bc7d93d4a2438b55edaa2ae022a63261c
Vishal Bhandare <vbhandare@webuy.com>
2020-05-03 23:48:57 +0530

    Changes to be committed: 	modified:   src/constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
 export const apiServiceUrl = 'https://rocky-spire-82610.herokuapp.com';
-export const clientID = 3;
-export const clientSecretKey= 'qDP0Ym6aJlJkjUuD0TEjI97yF5fiIjmLG516Zcdd';
+export const clientID = 1;
+export const clientSecretKey= 'qZtlntLsDnDOX3BNXLboGYiIcvnZYnse2NsMuutf';
 export const scope = '*';
